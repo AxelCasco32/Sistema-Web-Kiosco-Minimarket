8a93b88d6a3ad932fbe3e0a94eb4eb2c75ac3dc3
Tu Nombre <axelguillermocasco@gmail.com>
2026-09-16 19:59:46 -0300

    conexion a db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,3 +6,19 @@
 //   - Crear y exportar la instancia de conexión/ORM (ej: Sequelize) para que
 //     los modelos la importen.
 // No es la base de datos en sí, solo el puente de conexión hacia ella.
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false, // Desactivar logs de SQL en consola
+  }
+);
+
+module.exports = sequelize; 
